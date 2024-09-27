b8efd9b1f2a9f10d791a988f3d6fdeedb6ffe9bd
GalaxMax <galaxmax004@gmail.com>
2024-09-27 11:01:09 +0200

    innkapsling for Output
    La til Set og Get for input parameteren.

--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -1,5 +1,14 @@
 public class Output {
+    private String input;
     public Output(String input) {
-        
+        setInput(input);
     }
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return this.input;
+    }
+    
 }
