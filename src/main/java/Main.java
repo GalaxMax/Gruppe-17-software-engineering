95753fa769f241723d6196bdddcf05e07009d230
GalaxMax <galaxmax004@gmail.com>
2024-09-27 11:47:18 +0200

    La til nye metoder
    Output har nå en metode getOutputToInt som tar en string input og returnerer som en int.
    La til en Override for den innebygde toString metoden.
    
    La til noen midlertidige eksempler for bruk i Main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello world!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Output testOutput = new Output("4096");
+        System.out.println(testOutput);
+        System.out.println(testOutput.getOutputToInt());
     }
 }
