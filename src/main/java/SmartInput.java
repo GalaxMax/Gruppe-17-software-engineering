1da2464b142354057222e74c85ff54d42aa74208
HJ <hansjgr@hiof.no>
2024-09-29 20:25:46 +0200

    Endret hvordan man tar inn input fra integrert konsoll i IntelliJ til eget programmvindu. Skrevet opp noen dummy-keys som gir textbasert output i vinduet, men det er ikke koblet opp mot output-klassen enda.

--- src/main/java/SmartInput.java
+++ src/main/java/SmartInput.java
@@ -1,38 +1,100 @@
-package main.java;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class SmartInput {
 
+        JFrame FK; // FK = Fjern-Kontroll
+        JPanel panel;
+        JLabel textLabel;
+
         private Scanner deviceInput;
+
         public SmartInput() {
                 deviceInput = new Scanner(System.in);
+                Font font = new Font("Times New Roman", Font.PLAIN, 28);
+
+                KeyHandler kHandler = new KeyHandler();
+
+                FK = new JFrame();
+                FK.setSize(800,600);
+                FK.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                FK.getContentPane().setBackground(Color.black);
+                FK.setLayout(null);
+                FK.addKeyListener(kHandler);
+
+                panel = new JPanel();
+                panel.setBounds(100, 150, 600, 250);
+                panel.setBackground(Color.black);
+                panel.setLayout(new GridLayout(2,1));
+                FK.add(panel);
+
+                textLabel = new JLabel();
+                textLabel.setBackground(Color.black);
+                textLabel.setForeground(Color.white);
+                textLabel.setFont(font);
+                panel.add(textLabel);
+
+                FK.setVisible(true);
         }
+
+        public class KeyHandler implements KeyListener{
+
+                @Override
+                public void keyTyped(KeyEvent e) {
+
+                }
+
+                @Override
+                public void keyPressed(KeyEvent e) {
+
+                        switch(e.getKeyCode()){
+                                case KeyEvent.VK_1:
+                                        textLabel.setText("1!");
+                                        break;
+                                case KeyEvent.VK_2:
+                                        textLabel.setText("2. wow!");
+                                        break;
+                                case KeyEvent.VK_LEFT:
+                                        textLabel.setText("Det var venstre, gitt!");
+                                        break;
+                                case KeyEvent.VK_RIGHT:
+                                        textLabel.setText("Høyre, nice!");
+                                        break;
+                                case KeyEvent.VK_A:
+                                        textLabel.setText("AAAAAAAAAAAA");
+                                        break;
+                                default:
+                                        textLabel.setText("Kul knapp!");
+                                        break;
+                        }
+
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {
+
+                }
+        }
+
         public String readInput() {
                 System.out.print("press button");
                 return deviceInput.nextLine();
         }
+
         public void closeDevice() {
                 if (deviceInput != null) {
                         deviceInput.close();
                         System.out.println("bye.");
                 }
         }
-        public void keyPressed(KeyEvent e) {
-                int Key = e.getKeyCode();
-                switch (Key) {
-                        case KeyEvent.VK_LEFT:
-                                System.out.println("hello");
-                                break;
-                        case KeyEvent.VK_RIGHT:
-                                System.out.println("hello2");
-                                break;
-                }
-        }
-        public static void main(String[] args) {
-                SmartInput handler = new SmartInput();
-                String Smartinput = handler.readInput();
-                System.out.println(Smartinput);
-                handler.closeDevice();
-        }
-
 }
+
+
