4752f84fcb49454b6a4acbb4ed19a7391e2f8484
notveryhelpfuldude <caracass@hotmail.no>
2024-09-29 12:36:09 +0200

    Private Scanner

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
 public class Input {
+        // 
+        private Scanner deviceInput;
+        public Input() {
+                deviceInput = new Scanner(System.in);
+        }
 
-    
 }
