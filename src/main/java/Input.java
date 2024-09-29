7667a0efbc4c4d9f8af25e17ec58f4643cb16c0a
notveryhelpfuldude <caracass@hotmail.no>
2024-09-29 12:47:32 +0200

    test 1

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Input {
-        //
+        
         private Scanner deviceInput;
         public Input() {
                 deviceInput = new Scanner(System.in);
@@ -8,5 +8,10 @@ public class Input {
         public String readInput() {
                 System.out.print("press button");
                 return deviceInput.nextLine();
+        }
+        public static void main(String[] args) {
+                DeviceInputHandler handler = new DeviceInputHandler();
+                String input = handler.readInput();
+                System.out.println(input);
         }
 }
