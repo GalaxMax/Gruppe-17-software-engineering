25ae51295390c193066ec5a26bef2159099c5da5
notveryhelpfuldude <caracass@hotmail.no>
2024-09-29 12:43:53 +0200

    Read Input

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 public class Input {
-        // 
+        //
         private Scanner deviceInput;
         public Input() {
                 deviceInput = new Scanner(System.in);
         }
-
+        public String readInput() {
+                System.out.print("press button");
+                return deviceInput.nextLine();
+        }
 }
