cf99d7f5e86f37e2d8f20d94ba24bd6f3d49f654
notveryhelpfuldude <caracass@hotmail.no>
2024-09-29 15:07:35 +0200

    Key listener

--- src/main/java/SmartInput.java
+++ src/main/java/SmartInput.java
@@ -1,6 +1,8 @@
 package main.java;
+import java.awt.event.KeyEvent;
 import java.util.Scanner;
 public class SmartInput {
+
         private Scanner deviceInput;
         public SmartInput() {
                 deviceInput = new Scanner(System.in);
@@ -13,6 +15,17 @@ public class SmartInput {
                 if (deviceInput != null) {
                         deviceInput.close();
                         System.out.println("bye.");
+                }
+        }
+        public void keyPressed(KeyEvent e) {
+                int Key = e.getKeyCode();
+                switch (Key) {
+                        case KeyEvent.VK_LEFT:
+                                System.out.println("hello");
+                                break;
+                        case KeyEvent.VK_RIGHT:
+                                System.out.println("hello2");
+                                break;
                 }
         }
         public static void main(String[] args) {
