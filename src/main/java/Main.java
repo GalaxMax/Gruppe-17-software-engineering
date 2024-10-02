8f39c4c4371ddec1b68bb574d46a909d8fc03409
chojujansen <48697035+chojujansen@users.noreply.github.com>
2024-10-02 14:30:37 +0200

    xmlxmlxml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         IoTSystem IoT = new IoTSystem();
         new SmartInput(IoT);
+
+        SwingUtilities.invokeLater(() -> {
+            PhilipsHueAPI controller = new PhilipsHueAPI();
+            controller.createAndShowGUI(); // Kalle på metoden her
+        });
     }
 }
