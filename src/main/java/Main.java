c7711537f1221c198d44483d5f6e851e606e8a16
notveryhelpfuldude <caracass@hotmail.no>
2024-10-09 15:40:40 +0200

    update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         IoTSystem IoT = new IoTSystem();
         new SmartInput(IoT);
-        States states = new States();
         JSONObject joetest = new JSONObject();
 
         
