8090a114f1ab72fbb3d96e87b9b908ffe75b8600
notveryhelpfuldude <caracass@hotmail.no>
2024-10-09 11:44:30 +0200

    state class variables

--- src/main/java/States.java
+++ src/main/java/States.java
@@ -1,3 +1,8 @@
 public class States {
-
+    public double brightness;
+    public double volume;
+    public double hue;
+    public double saturation;
+    public double temperature;
+    public double humidity;
 }
