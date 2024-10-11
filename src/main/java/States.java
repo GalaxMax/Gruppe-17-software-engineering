193c21d80f451b175ea4567ec3ae2225cfc16941
notveryhelpfuldude <caracass@hotmail.no>
2024-10-11 11:20:49 +0200

    States class done

--- src/main/java/States.java
+++ src/main/java/States.java
@@ -2,20 +2,14 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class States {
+public abstract class States {
     public double brightness;
     public double volume;
     public double hue;
     public double saturation;
     public double temperature;
     public double humidity;
-    public States(double brightness, double volume, double hue, double saturation, double temperature, double humidity) {
-        this.brightness = brightness;
-        this.volume = volume;
-        this.hue = hue;
-        this.saturation = saturation;
-        this.temperature = temperature;
-        this.humidity = humidity;
+    public States() {
         //Will try to read a JSON file and read the contents of the file line by line
         //then add said content to a string with key value pairs which is added to a json object
         //if it can't find the file, it will catch an exception
@@ -27,21 +21,15 @@ public class States {
                 jsonContent.append(line);
             }
             JSONObject jsonObject = new JSONObject(jsonContent.toString());
-            System.out.println("Brightness: " + jsonObject.getDouble("brightness"));
-
-            /*
-            States states = new States((double) jsondata.get("brightness"),100, 100, 100,100, 100);
-
-            System.out.println("Volume: " + states.volume);
-            System.out.println("Hue: " + states.hue);
-            System.out.println("Saturation: " + states.saturation);
-            System.out.println("Temperature: " + states.temperature);
-            System.out.println("Humidity: " + states.humidity);
-
-             */
-            //System.out.println("Humidity: " + jsondata.getDouble("humidity"));
+            // closes the reader to save resources
             reader.close();
-            System.out.println("contents " + reader);
+            //Assign the values found in the json to the variables in the object
+            this.brightness = jsonObject.getDouble("brightness");
+            this.volume = jsonObject.getDouble("volume");
+            this.hue = jsonObject.getDouble("hue");
+            this.saturation = jsonObject.getDouble("saturation");
+            this.temperature = jsonObject.getDouble("temperature");
+            this.humidity = jsonObject.getDouble("humidity");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
