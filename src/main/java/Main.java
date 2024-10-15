b88ea62568ca7b39b8b37f4d73d714a66012f10a
notveryhelpfuldude <caracass@hotmail.no>
2024-10-15 13:38:03 +0200

    cleaning up main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,5 @@ public class Main {
         IoTSystem IoT = new IoTSystem();
         new SmartInput(IoT);
         new PhilipsHueAPI();
-        RemotePreset_1 preset_1 = new RemotePreset_1();
-        RemotePreset_2 preset_2= new RemotePreset_2();
-        System.out.println(preset_1.hue);
-        System.out.println(preset_2.hue);
     }
 }
