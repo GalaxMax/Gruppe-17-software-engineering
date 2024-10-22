78a10e541b0592d18a82dd3b7b04bbb268465a6f
GalaxMax <galaxmax004@gmail.com>
2024-10-22 13:13:01 +0200

    Prepare to merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,7 @@ import output.ActiveOutputs;
 
 public class Main {
     public static void main(String[] args) {
-
         ActiveOutputs IoTSystems = new ActiveOutputs();
         new SmartInput(IoTSystems);
-
-        //IoTSystem IoT = new IoTSystem();
-        //new SmartInput(IoT);
-
-        //SwingUtilities.invokeLater(() -> {
-        //    PhilipsHueAPI controller = new PhilipsHueAPI();
-        //    controller.createAndShowGUI(); // Kalle på metoden her
-        //});
     }
 }
