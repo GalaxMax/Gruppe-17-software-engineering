0e2e0fa3ae45fe7016b8a40fd7b92c8a00425a6a
notveryhelpfuldude <caracass@hotmail.no>
2024-10-23 17:48:26 +0200

    Json test Setup

--- src/test/java/remotePresets/JsonTest.java
+++ src/test/java/remotePresets/JsonTest.java
@@ -2,6 +2,11 @@ package remotePresets;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 class Remote_test extends RemotePresets {
     public Remote_test(String jsonfile) {
@@ -11,10 +16,17 @@ class Remote_test extends RemotePresets {
 public class JsonTest {
 
     private String validJsonFilePath;
+    private String invalidJsonFilePath;
 
+    @BeforeEach
+    public void setUp() throws IOException {
+        validJsonFilePath = "validRemotePresets.json";
+        BufferedWriter writer = new BufferedWriter(new FileWriter(validJsonFilePath));
+        invalidJsonFilePath = "nonExistentFile.json";
+    }
     @Test
     public void jsontest() {
         Remote_test remotePreset2_test = new Remote_test(validJsonFilePath);
-        //Assertions.assertEquals();
+        Assertions.assertEquals();
     }
 }
