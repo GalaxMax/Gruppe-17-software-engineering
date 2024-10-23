69fd0a3c077e8585a2ec9ba8345ad874c6deb67c
notveryhelpfuldude <caracass@hotmail.no>
2024-10-23 16:21:18 +0200

    La til JsonTest

---
+++ src/test/java/remotePresets/JsonTest.java
@@ -0,0 +1,12 @@
+package remotePresets;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JsonTest {
+
+    @Test
+    public void jsonIn() {
+        //Assertions.assertEquals();
+    }
+}
