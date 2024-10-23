3aa3ec97bef579e838cdc5a654d97840ed1c5afc
notveryhelpfuldude <caracass@hotmail.no>
2024-10-23 17:41:43 +0200

    JsonTest med filepath

--- src/test/java/remotePresets/JsonTest.java
+++ src/test/java/remotePresets/JsonTest.java
@@ -3,10 +3,18 @@ package remotePresets;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+class Remote_test extends RemotePresets {
+    public Remote_test(String jsonfile) {
+        super(jsonfile);
+    }
+}
 public class JsonTest {
 
+    private String validJsonFilePath;
+
     @Test
-    public void jsonIn() {
+    public void jsontest() {
+        Remote_test remotePreset2_test = new Remote_test(validJsonFilePath);
         //Assertions.assertEquals();
     }
 }
