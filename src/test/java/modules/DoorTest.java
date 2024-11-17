1f98c7c305eeace6e812cb781b0e9453405fae54
GalaxMax <galaxmax004@gmail.com>
2024-11-17 15:28:38 +0100

    Made DoorTest with Unlock testing

---
+++ src/test/java/modules/DoorTest.java
@@ -0,0 +1,13 @@
+package modules;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DoorTest {
+    @Test
+    public void doorUnlock() {
+        Door door = new Door("DoorTest");
+        door.doorUnlock();
+        Assertions.assertTrue(door.state);
+    }
+}
