5971cc2dfba8f0b4f85799763d08890dcda395b2
GalaxMax <galaxmax004@gmail.com>
2024-11-17 15:46:49 +0100

    Added ModuleTemplateTest and a state tests

---
+++ src/test/java/modules/ModuleTemplateTest.java
@@ -0,0 +1,13 @@
+package modules;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ModuleTemplateTest {
+    @Test
+    public void setStateTestTrue() {
+        ModuleTemplate moduleTemplateTest = new Door("moduleTemplateTest"); //ModuleTemplate is abstract, so instead initialize one of the children
+        moduleTemplateTest.setState(true);
+        Assertions.assertTrue(moduleTemplateTest.state);
+    }
+}
