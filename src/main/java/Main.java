84612d5c3eefca8d30a1c1fee26b1ac2213cde42
notveryhelpfuldude <caracass@hotmail.no>
2024-11-19 12:48:39 +0100

    Main Clean up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,5 @@ public class Main {
     public static void main(String[] args) {
         ActiveModules IoTSystems = new ActiveModules();
         new SmartInput(IoTSystems);
-        RemotePresets test1 = new RemotePresets();
-        System.out.println(test1.brightness);
-        test1.SaveToJson(60.0, 7.0, 8.0, 9.0, 10.0, 11.0);
     }
 }
