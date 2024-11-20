f3b311140c3b79031afa5a44dc6ee70f62da1235
GalaxMax <galaxmax004@gmail.com>
2024-11-20 15:27:46 +0100

    Changed comment in LampDimmable

--- src/test/java/modules/LampDimmableTest.java
+++ src/test/java/modules/LampDimmableTest.java
@@ -16,7 +16,7 @@ public class LampDimmableTest {
     @Test
     public void lightDimDownTest() {  //tests for the dimming limit
         LampDimmable lampDimmableTest = new LampDimmable("LampDimTest");
-        for (int i = 0; i < 20; i++) {  //brightness should not go beyond 255, 17*20=340
+        for (int i = 0; i < 20; i++) {  //brightness should not go below 0, -17*20=-340
             lampDimmableTest.lightDimDown();
         }
         Assertions.assertEquals(0, lampDimmableTest.getBrightness());
