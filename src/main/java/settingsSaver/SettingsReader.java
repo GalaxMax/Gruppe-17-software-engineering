fa713ddeb4b432f3624c9725452042a6c87262ce
notveryhelpfuldude <caracass@hotmail.no>
2024-11-21 20:12:24 +0100

    Imrovements to Json classes and tests

--- src/main/java/settingsSaver/SettingsReader.java
+++ src/main/java/settingsSaver/SettingsReader.java
@@ -16,12 +16,12 @@ public class SettingsReader {
 
         File file = new File(filePath);
         if(file.exists()) {
-            try {
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath));){
                 //Will try to read a JSON file and read the contents of the file line by line
                 //then format and add said content to a hashmap
                 //if it can't find the file, it will catch an exception
                 System.out.println("Reading file " + filePath);
-                BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
                 String line;
                 while ((line = reader.readLine()) != null) {
                     line = line.trim();
@@ -41,10 +41,7 @@ public class SettingsReader {
 
                         savedSettings.put(key, Integer.valueOf(value));
                     }
-
                 }
-                //closes the reader to save resources
-                reader.close();
 
             } catch (Exception e) {throw new RuntimeException(e);}
         }
