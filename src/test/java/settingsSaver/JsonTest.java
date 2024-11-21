193742ba261d93dd991e935de2d9d33dfbcbfc7e
notveryhelpfuldude <caracass@hotmail.no>
2024-11-21 15:30:50 +0100

    Test: Checks if file is created

--- src/test/java/settingsSaver/JsonTest.java
+++ src/test/java/settingsSaver/JsonTest.java
@@ -6,19 +6,25 @@ import org.junit.jupiter.api.BeforeEach;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static settingsSaver.SettingsWriter.writeJSON;
 
 public class JsonTest {
-
-    private String validJsonFilePath;
-    private String invalidJsonFilePath;
-
+    String testFile = "test1";
     //Benytter @BeforeEach for å sørge for at filen lages før den testes
     @BeforeEach
     public void setUp() throws IOException {
-
+        HashMap<String, Integer> jsontest = new HashMap<>();
+        jsontest.put("Test1", 1);
+        writeJSON(testFile,jsontest);
     }
     @Test
     public void jsontest() {
+        assertTrue(Files.exists(Paths.get("src/Main/JSON/" + testFile +".json")));
 
     }
+
 }
