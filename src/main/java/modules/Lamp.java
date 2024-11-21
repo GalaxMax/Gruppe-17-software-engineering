6345d3b498eda83925cb6053da6b021b45fecaeb
chojujansen <48697035+chojujansen@users.noreply.github.com>
2024-11-21 23:55:25 +0100

    Endret det visuelle
    Legg til ikoner i stedet for firkanter for lampene:
    
        Bruker lampens ikoner for å vise status i stedet for firkantede bakgrunner.
    
    Legg til dimming-funksjonalitet for LampDimmable-ikon:
    
        Justerbar lysstyrke som endrer ikonet basert på innstillingene.

--- src/main/java/modules/Lamp.java
+++ src/main/java/modules/Lamp.java
@@ -4,54 +4,67 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Lamp extends ModuleTemplate {
-    public Lamp(String name){
-        this.name=name;
+    protected JLabel label;
+    private ImageIcon lampOffIcon;
+    private ImageIcon lampOnIcon;
+
+    public Lamp(String name) {
+        this.name = name;
         lampOutput(name);
-        if(loadSettings()) refresh();
-    }
-    public Lamp(String name, TextModule terminal){  //overloading if terminal access is wanted for the object
-        super(terminal);
-        this.name=name;
-        lampOutput(name);
-        if(loadSettings()) refresh();
+        if (loadSettings()) refresh();
     }
 
-    private void lampOutput(String name){
+    public Lamp(String name, TextModule terminal) {
+        super(terminal);
+        this.name = name;
+        lampOutput(name);
+        if (loadSettings()) refresh();
+    }
+
+    private void lampOutput(String name) {
+        lampOffIcon = new ImageIcon(new ImageIcon("src/main/resources/icons/lampOff.png")
+                .getImage().getScaledInstance(200, 150, Image.SCALE_SMOOTH));
+        lampOnIcon = new ImageIcon(new ImageIcon("src/main/resources/icons/lampOn.png")
+                .getImage().getScaledInstance(200, 150, Image.SCALE_SMOOTH));
+
         outputWindow = new JFrame(name);
-        outputWindow.setSize(400,300);
+        outputWindow.setSize(400, 300);
         outputWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        outputWindow.getContentPane().setBackground(Color.black);
         outputWindow.setLayout(null);
 
-        label = new JPanel();
+        label = new JLabel();
         label.setBounds(100, 75, 200, 150);
-        label.setBackground(Color.black);
+        label.setIcon(lampOffIcon);
         outputWindow.add(label);
 
         outputWindow.setVisible(true);
     }
 
+
+
     public void lightOn() {
-        label.setBackground(Color.white);
+        label.setIcon(lampOnIcon);
         terminalAccess("Lys på");
         setState(true);
     }
+
     public void lightOff() {
-        label.setBackground(Color.black);
+        label.setIcon(lampOffIcon);
         terminalAccess("Lys av");
         setState(false);
     }
+
     public void toggleLight() {
         if (this.state) {
             lightOff();
+        } else {
+            lightOn();
         }
-        else lightOn();
     }
 
     @Override
-    protected void refresh(){
-        if(getState()) lightOn();
+    protected void refresh() {
+        if (getState()) lightOn();
         else lightOff();
     }
-
 }
